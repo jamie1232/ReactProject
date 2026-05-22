95d2bf81661833560f758b1909f7e8d2c0859b17
jamie1232 <74980455+jamie1232@users.noreply.github.com>
2026-05-22 06:31:55 +0100

    -

--- student-workspace/backend/workspace/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Team, Membership, Task, Meeting
-
-admin.site.register(Team)
-admin.site.register(Membership)
-admin.site.register(Task)
-admin.site.register(Meeting)
